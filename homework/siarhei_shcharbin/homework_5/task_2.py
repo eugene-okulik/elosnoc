b2f69317b276192162d64a3b11d6e848128e09d5
Siarhei Shcharbin <oleelosnoc@gmail.com>
2024-12-10 13:47:53 +0400

    improved code for task_2 in order to unify and simplify work with different inputs in homework_5

--- homework/siarhei_shcharbin/homework_5/task_2.py
+++ homework/siarhei_shcharbin/homework_5/task_2.py
@@ -1,9 +1,9 @@
-a = 'результат операции: 42'
-b = 'результат операции: 514'
-c = 'результат работы программы: 9'
-a_num_pos = a.index('42')
+# Values for input are:
+# 'результат операции: 42'
+# 'результат операции: 514'
+# 'результат работы программы: 9'
+
+
+a = input()
+a_num_pos = int(a.index(':')) + 2
 print(int(a[a_num_pos:]) + 10)
-b_num_pos = b.index('514')
-print(int(b[b_num_pos:]) + 10)
-c_num_pos = c.index('9')
-print(int(c[c_num_pos:]) + 10)
